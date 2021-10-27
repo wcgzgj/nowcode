dd96bf6c25520e8e33fa735aefecdebc33cae544
wcgzgj <zhangjia_good@126.com>
2021-10-27 15:40:05 +0800

    m

--- src/must/ques/CC18_cloneGraph.java
+++ src/must/ques/CC18_cloneGraph.java
@@ -1,6 +1,8 @@
 package must.ques;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @ClassName CC18_cloneGraph
@@ -29,13 +31,48 @@ public class CC18_cloneGraph {
         solu.cloneGraph(n0);
     }
 
+    // 这种单纯的 dfs 解法有一个致命的问题，那就是会出现重复新建对象的问题
+    //public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+    //    UndirectedGraphNode newNode = new UndirectedGraphNode(node.label);
+    //    if (node.neighbors.size()==0) return newNode;
+    //    for (UndirectedGraphNode neighbor : node.neighbors) {
+    //        if (node==neighbor) continue;
+    //        UndirectedGraphNode newNeighbor = cloneGraph(neighbor);
+    //        if (newNeighbor!=null) newNode.neighbors.add(newNeighbor);
+    //    }
+    //    return newNode;
+    //}
+
+    // 我们需要使用 map 存储源对象和新对象的关系，来判断该对象之前有没有被新建过
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
-        UndirectedGraphNode newNode = new UndirectedGraphNode(node.label);
-        if (node.neighbors.size()==0) return newNode;
-        for (UndirectedGraphNode neighbor : node.neighbors) {
-            if (node==neighbor) continue;
-            UndirectedGraphNode newNeighbor = cloneGraph(neighbor);
-            if (newNeighbor!=null) newNode.neighbors.add(newNeighbor);
+        if (node ==null) return null;
+        // key: 原图节点对象
+        // value: 新图节点对象
+        Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+        return dfs(map,node);
+    }
+
+    public UndirectedGraphNode dfs(Map<UndirectedGraphNode, UndirectedGraphNode> map,UndirectedGraphNode node) {
+
+        UndirectedGraphNode newNode = null;
+        if (map.get(node)==null) {
+             newNode = new UndirectedGraphNode(node.label);
+             map.put(node,newNode);
+        } else {
+            newNode = map.get(node);
+        }
+        if (node.neighbors.size()>0) {
+            for (UndirectedGraphNode neighbor : node.neighbors) {
+                if (map.get(neighbor)==null) {
+                    // 有的话，直接从 map 取出，避免重复创建对象
+                    // 无则创建，并且别忘了再放回 map
+                    UndirectedGraphNode newNeighbor = new UndirectedGraphNode(node.label);
+                    // 注：这里可能会出现循环引用问题 2->2
+                    map.put(neighbor,newNeighbor);
+                }
+                if (newNode.label!= neighbor.label) dfs(map,neighbor);
+                newNode.neighbors.add(map.get(neighbor));
+            }
         }
         return newNode;
     }
--- src/must/ques/CC19_minCut.java
+++ src/must/ques/CC19_minCut.java
@@ -10,20 +10,39 @@ package must.ques;
 public class CC19_minCut {
     public static void main(String[] args) {
         CC19_minCut solu = new CC19_minCut();
-        System.out.println(solu.isHui("abcba",0,3));
+        System.out.println(solu.minCut("abbab"));
     }
 
     public int minCut (String s) {
-        return -1;
+        int[] dp = new int[s.length()];
+        for (int i = 0; i < dp.length; i++) {
+            // 获得
+            int startIndex = getMinDPStartIndex(s, i,dp);
+            // 从头部到e 就是一个回文串，说明直到 e 都不需要进行分割
+            if (startIndex==0) dp[i]=0;
+            // 状态转移方程
+            else dp[i]=dp[startIndex-1]+1;
+        }
+        return dp[dp.length-1];
     }
 
-    // 获取末尾为 e 的最长的回文子串的头部的 index
-    public int getMaxHuiStartIndex(String s,int e) {
+    // 在所有 s-e 可能形成回文的情况中，选出 dp[s-1] 最小的那一个
+    public int getMinDPStartIndex(String s,int e,int[] dp) {
+        int res = 0;
+        int minDP = Integer.MAX_VALUE;
         for (int i = 0; i <= e; i++) {
-            if (isHui(s,i,e)) return i;
+            if (isHui(s,i,e)) {
+                // 如果和第一个元素就能形成回文串，那么都不用考虑了，这种情况就是最优的
+                // ，因为连拆都不用拆
+                if (i==0) return 0;
+                if (dp[i-1]<minDP) {
+                    minDP=dp[i-1];
+                    res = i;
+                }
+            }
         }
         // 最差情况，就是返回 e
-        return e;
+        return res;
     }
 
     public boolean isHui(String str,int l,int r) {
