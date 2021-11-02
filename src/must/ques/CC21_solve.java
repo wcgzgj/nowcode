ad334bcce85f49e75bc5f692ecb74e420824cb46
wcgzgj <zhangjia_good@126.com>
2021-11-02 13:06:54 +0800

    m

---
+++ src/must/ques/CC21_solve.java
@@ -0,0 +1,25 @@
+package must.ques;
+
+/**
+ * 现在有一个仅包含‘X’和‘O’的二维板，请捕获所有的被‘X’包围的区域
+ * 捕获一个被包围区域的方法是将被包围区域中的所有‘O’变成‘X’
+ * 例如
+ * X X X X
+ * X O O X
+ * X X O X
+ * O X X X
+ * 执行完你给出的函数以后，这个二维板应该变成：
+ * X X X X
+ * X X X X
+ * X X X X
+ * O X X X
+ **/
+public class CC21_solve {
+    public static void main(String[] args) {
+
+    }
+
+    public void solve(char[][] board) {
+
+    }
+}
