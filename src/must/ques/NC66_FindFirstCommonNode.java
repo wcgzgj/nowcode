0ad37da83d04f8fdbe05d69a6b07d3185f145d3a
wcgzgj <zhangjia_good@126.com>
2021-11-15 13:17:16 +0800

    链表公共节点(聪明代码)

--- src/must/ques/NC66_FindFirstCommonNode.java
+++ src/must/ques/NC66_FindFirstCommonNode.java
@@ -14,38 +14,52 @@ public class NC66_FindFirstCommonNode {
 
     }
 
-    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
-        int len1 = countListLength(pHead1);
-        int len2 = countListLength(pHead2);
-        int len = Math.abs(len1 - len2);
-        ListNode[] nodes = new ListNode[2];
-        if (len1>=len2) {
-            nodes[0]=pHead1;
-            nodes[1]=pHead2;
-        } else {
-            nodes[0]=pHead2;
-            nodes[1]=pHead1;
-        }
-        ListNode longHead = nodes[0];
-        ListNode shortHead = nodes[1];
-        while (len-->0) {
-            longHead=longHead.next;
-        }
-        while (longHead!=null) {
-            if (longHead==shortHead) return longHead;
-            longHead=longHead.next;
-            shortHead=shortHead.next;
-        }
-        return null;
-    }
+    // 方法1：统计链表长度，让长的那个先走完差值
+    //public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+    //    int len1 = countListLength(pHead1);
+    //    int len2 = countListLength(pHead2);
+    //    int len = Math.abs(len1 - len2);
+    //    ListNode[] nodes = new ListNode[2];
+    //    if (len1>=len2) {
+    //        nodes[0]=pHead1;
+    //        nodes[1]=pHead2;
+    //    } else {
+    //        nodes[0]=pHead2;
+    //        nodes[1]=pHead1;
+    //    }
+    //    ListNode longHead = nodes[0];
+    //    ListNode shortHead = nodes[1];
+    //    while (len-->0) {
+    //        longHead=longHead.next;
+    //    }
+    //    while (longHead!=null) {
+    //        if (longHead==shortHead) return longHead;
+    //        longHead=longHead.next;
+    //        shortHead=shortHead.next;
+    //    }
+    //    return null;
+    //}
+    //
+    //private int countListLength(ListNode head) {
+    //    if (head==null) return 0;
+    //    int count=0;
+    //    while (head!=null) {
+    //        ++count;
+    //        head=head.next;
+    //    }
+    //    return count;
+    //}
 
-    private int countListLength(ListNode head) {
-        if (head==null) return 0;
-        int count=0;
-        while (head!=null) {
-            ++count;
-            head=head.next;
+
+
+    // 相互补上两个链表的长度，让二者的长度一致
+    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+        ListNode ta = pHead1;
+        ListNode tb = pHead2;
+        while (ta!=tb) {
+            ta=ta==null?pHead2:ta.next;
+            tb=tb==null?pHead1:tb.next;
         }
-        return count;
+        return ta;
     }
 }
