673fff0a97ec26340541a6b5768a5941254ce173
wcgzgj <zhangjia_good@126.com>
2021-11-17 16:10:41 +0800

    最长回文子串（动态规划版）

--- src/must/ques/NC17_getLongestPalindrome.java
+++ src/must/ques/NC17_getLongestPalindrome.java
@@ -10,7 +10,7 @@ package must.ques;
 public class NC17_getLongestPalindrome {
     public static void main(String[] args) {
         NC17_getLongestPalindrome solu = new NC17_getLongestPalindrome();
-        System.out.println(solu.getLongestPalindrome("baabccc",7));
+        System.out.println(solu.getLongestPalindrome("baabccc"));
     }
 
     // 方法1：遍历，时间复杂度为 O(n^2)
@@ -34,22 +34,28 @@ public class NC17_getLongestPalindrome {
     //}
 
     // 方法2 动态规划
-    public int getLongestPalindrome(String A, int n) {
+    public int getLongestPalindrome(String A) {
+        if (A.length()<=1) return A.length();
+        int n = A.length();
         int[][] dp = new int[n][n];
-        int res = Integer.MIN_VALUE;
-        for (int l = 0; l < n; l++) {
-            for (int r = l; r <n ; r++) {
-                if (l==r) dp[l][r]=1;
-                else if (A.charAt(l)==A.charAt(r)) {
+        int res = 1;
+        int len = A.length();
+        // 长度为 1 的串，肯定是回文的
+        for (int i = 0; i < n; i++) {
+            dp[i][i]=1;
+        }
+        // 将长度为 2-str.length 的所有串遍历一遍
+        for (int step = 2; step <= A.length(); step++) {
+            for (int l = 0; l < A.length(); l++) {
+                int r=l+step-1;
+                if (r>=len) break;
+                if (A.charAt(l)==A.charAt(r)) {
                     if (r-l==1) dp[l][r]=2;
-                    else {
-                        if (dp[l+1][r-1]>0) dp[l][r]=dp[l+1][r-1]+2;
-                    }
+                    else dp[l][r]=dp[l+1][r-1]==0?0:dp[l+1][r-1]+2;
                 }
                 res=Math.max(res,dp[l][r]);
             }
         }
-        outGrid(dp);
         return res;
     }
 
