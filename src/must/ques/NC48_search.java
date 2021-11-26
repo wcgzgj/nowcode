ea3c250c859b1040ddf914eb608f3c86656a973e
wcgzgj <zhangjia_good@126.com>
2021-11-26 19:40:38 +0800

    找数

---
+++ src/must/ques/NC48_search.java
@@ -0,0 +1,27 @@
+package must.ques;
+
+/**
+ * @ClassName NC48_search
+ * @Description TODO
+ * @Author faro_z
+ * @Date 2021/11/26 5:02 下午
+ * @Version 1.0
+ **/
+public class NC48_search {
+    public static void main(String[] args) {
+
+    }
+
+    //方法1： 额，这个做法是个人都能想到
+    //public int search (int[] nums, int target) {
+    //    for (int i = 0; i < nums.length; i++) {
+    //        if (nums[i]==target) return i;
+    //    }
+    //    return -1;
+    //}
+
+    // 方法2：二分
+    //public int search (int[] nums, int target) {
+    //
+    //}
+}
