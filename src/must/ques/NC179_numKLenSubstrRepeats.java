00bd5c45a5703b91ef1211c9b3202c431a9025c9
wcgzgj <zhangjia_good@126.com>
2022-01-15 15:40:02 +0800

    m

--- src/must/ques/NC179_numKLenSubstrRepeats.java
+++ src/must/ques/NC179_numKLenSubstrRepeats.java
@@ -15,27 +15,6 @@ public class NC179_numKLenSubstrRepeats {
         System.out.println(solu.numKLenSubstrRepeats("yokagames",3));
     }
 
-    //public int numKLenSubstrRepeats (String s, int k) {
-    //    if (k>s.length()) return 0;
-    //    int l=0,r=l+k-1;
-    //    int count = 0;
-    //    while (r<s.length()) {
-    //        if (hasSameWords(s,l++,r++)) count++;
-    //    }
-    //    return count;
-    //}
-    //
-    //public boolean hasSameWords(String s,int l,int r) {
-    //    HashSet<Character> set = new HashSet<>();
-    //    for (int i = l; i <=r ; i++) {
-    //        if (set.contains(s.charAt(i))) return true;
-    //        set.add(s.charAt(i));
-    //    }
-    //    return false;
-    //}
-
-    private static final HashSet<Character> set = new HashSet<>();
-
     public int numKLenSubstrRepeats (String s, int k) {
         if (k>s.length()) return 0;
         int l=0,r=l+k-1;
