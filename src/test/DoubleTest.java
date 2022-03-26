4bc4958dde571dcf95ecb4715e2eac2a079451ec
wcgzgj <zhangjia_good@126.com>
2022-03-26 19:19:34 +0800

    m

---
+++ src/test/DoubleTest.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * @ClassName DoubleTest
+ * @Description TODO
+ * @Author faro_z
+ * @Date 2022/3/26 7:17 下午
+ * @Version 1.0
+ **/
+public class DoubleTest {
+    public static void main(String[] args) {
+        double num1 = 0.3;
+        double num2 = 0.2;
+        System.out.println(num1 - num2);
+    }
+}
