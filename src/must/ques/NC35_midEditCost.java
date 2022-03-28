782bb164a7362072159956de8b3200e549871986
wcgzgj <zhangjia_good@126.com>
2022-03-28 14:20:51 +0800

    编辑距离(2) 牛客

--- src/must/ques/NC35_midEditCost.java
+++ src/must/ques/NC35_midEditCost.java
@@ -23,8 +23,29 @@ public class NC35_midEditCost {
      * @return
      */
     public int minEditCost (String str1, String str2, int ic, int dc, int rc) {
-        // write code here
-
-        return -1;
+        int R = str1.length() + 1;
+        int C = str2.length() + 1;
+        int[][] dp = new int[R][C];
+        dp[0][0] = 0;
+        for (int i = 1; i < C; i++) {
+            dp[0][i] = dp[0][i - 1] + ic;
+        }
+        for (int i = 1; i < R; i++) {
+            dp[i][0] = dp[i - 1][0] + dc;
+        }
+        for (int i = 1; i <R ; i++) {
+            for (int j = 1; j < C; j++) {
+                int del = dp[i - 1][j] + dc;
+                int ins = dp[i][j - 1] + ic;
+                int cha = dp[i - 1][j - 1] +rc;
+                int min = Math.min(Math.min(del, ins), cha);
+                if (str1.charAt(i-1)==str2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = min;
+                }
+            }
+        }
+        return dp[R-1][C-1];
     }
 }
