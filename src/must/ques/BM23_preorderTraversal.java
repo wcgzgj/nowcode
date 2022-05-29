c0a3f1980e91e8b043cd1dc847e025f44c7683c1
wcgzgj <zhangjia_good@126.com>
2022-05-29 18:46:30 +0800

    栈代替递归

---
+++ src/must/ques/BM23_preorderTraversal.java
@@ -0,0 +1,55 @@
+package must.ques;
+
+import must.domain.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @ClassName BM23_preorderTraversal
+ * @Description TODO
+ * @Author faro_z
+ * @Date 2022/5/28 5:32 下午
+ * @Version 1.0
+ **/
+public class BM23_preorderTraversal {
+
+    // 方法1：递归
+    //public int[] preorderTraversal (TreeNode root) {
+    //    List<Integer> res = new ArrayList<>();
+    //    dfs(res,root);
+    //    int[] resArr = new int[res.size()];
+    //    for (int i = 0; i < res.size(); i++) {
+    //        resArr[i]=res.get(i);
+    //    }
+    //    return resArr;
+    //}
+    //
+    //private void dfs(List<Integer>res,TreeNode root) {
+    //    if (root==null) return;
+    //    res.add(root.val);
+    //    dfs(res,root.left);
+    //    dfs(res,root.right);
+    //}
+
+    // 方法2：使用栈
+    public int[] preorderTraversal (TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> res = new ArrayList<>();
+        if (root==null) return new int[0];
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode tmp = stack.pop();
+            res.add(tmp.val);
+            if (tmp.right!=null) stack.push(tmp.right);
+            if (tmp.left!=null) stack.push(tmp.left);
+        }
+        int[] resArr = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            resArr[i]=res.get(i);
+        }
+        return resArr;
+    }
+}
