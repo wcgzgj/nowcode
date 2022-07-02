60b9db4868b6ab10222b4b9e05f17cdf4dab0363
wcgzgj <zhangjia_good@126.com>
2022-07-02 14:52:51 +0800

    m

---
+++ src/must/ques/leetcode221_maximalSquare.java
@@ -0,0 +1,19 @@
+package must.ques;
+
+/**
+ * @ClassName leetcode221_maximalSquare
+ * @Description TODO
+ * @Author faro_z
+ * @Date 2022/6/22 18:45
+ * @Version 1.0
+ **/
+public class leetcode221_maximalSquare {
+    public static void main(String[] args) {
+
+    }
+
+    public int maximalSquare(char[][] matrix) {
+
+        return -1;
+    }
+}
---
+++ src/test/DateTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.util.Date;
+
+/**
+ * @ClassName DateTest
+ * @Description TODO
+ * @Author faro_z
+ * @Date 2022/6/29 17:18
+ * @Version 1.0
+ **/
+public class DateTest {
+    public static void main(String[] args) {
+        Long zero = new Long(0);
+        System.out.println(zero.equals(0));
+        System.out.println(zero.equals(new Long(0)));
+    }
+}
